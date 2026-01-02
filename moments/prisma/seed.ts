e1bd468642bf637576c00401f36206007b33a99f
aryamantepal <aryamandora@gmail.com>
2026-01-03 00:18:52 +0400

    moment id

--- moments/prisma/seed.ts
+++ moments/prisma/seed.ts
@@ -27,7 +27,7 @@ const userData: Prisma.UserCreateInput[] = [
                 },
             ],
         },
-        Moment: {
+        moments: {
             create: [
                 {
                     imageUrl: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4",
@@ -54,7 +54,7 @@ const userData: Prisma.UserCreateInput[] = [
                 },
             ],
         },
-        Moment: {
+        moments: {
             create: [
                 {
                     imageUrl: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
